266a4ffaac7cdf7d8cfeca97105df5d7a44916dd
euanb <c1021396@ncl.ac.uk>
2023-04-10 16:31:38 +0100

    final commit

--- src/main/java/dev/euan/movies/MoviesApplication.java
+++ src/main/java/dev/euan/movies/MoviesApplication.java
@@ -2,8 +2,10 @@ package dev.euan.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin(origins = "*")
 public class MoviesApplication {
 
 	public static void main(String[] args) {
